cd290ea6f95783c63569e65c1b7822ebb0bd7448
freedomshare <60738260+freedomshare@users.noreply.github.com>
2022-02-06 23:35:02 -0500

    Update Social.tsx

--- src/components/Sidebar/Social.tsx
+++ src/components/Sidebar/Social.tsx
@@ -4,19 +4,19 @@ import { Icon } from "@olympusdao/component-library";
 export default function Social() {
   return (
     <div className="social-row">
-      <Link href="https://github.com/OlympusDAO" target="_blank">
-        <Icon name={"github"} />
+      <Link href="https://t.me/dapp4sale" target="_blank">
+        <Icon name={"buy now"} />
       </Link>
 
-      <Link href="https://olympusdao.medium.com/" target="_blank">
+      <Link href="https://t.me/dapp4sale" target="_blank">
         <Icon name={"medium"} />
       </Link>
 
-      <Link href="https://twitter.com/OlympusDAO" target="_blank">
+      <Link href="https://t.me/dapp4sale" target="_blank">
         <Icon name={"twitter"} />
       </Link>
 
-      <Link href="https://discord.gg/6QjjtUcfM4" target="_blank">
+      <Link href="https://t.me/dapp4sale" target="_blank">
         <Icon name={"discord"} />
       </Link>
     </div>
